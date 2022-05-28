455df8013e7a9b138012d153bbe0bc3bbf514505
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2022-05-29 02:05:53 +0530

    Completed Frontend of login and home page

---
+++ src/components/Main.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Main = (props) => {
+  return (
+    <Container>
+        <ShareBox>
+          Share
+          <div>
+            <img src='/images/user.svg' alt="" />
+            <button>
+              Start a post
+            </button>
+          </div>
+
+          <div id= "options">
+            <button>
+              <img src='/images/photo-icon.png'  alt='' />
+              <span>Photo</span>
+            </button>
+
+            <button>
+              <img src="/images/video-icon.png" alt=""></img>
+              <span>Video</span>
+            </button>
+
+            <button>
+              <img src="/images/event-icon.png"  alt=""></img>
+              <span>Job</span>
+            </button>
+
+            <button>
+              <img src="/images/article-icon.png" alt=""></img>
+              <span>Write article</span>
+            </button>
+          </div>
+        </ShareBox>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgba(0 0 0 /15%), 0 0 0 rgba(0 0 0 /20%) ;
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+  #options img{
+    width: 20px;
+    height: 16px;
+  }
+  div{
+    button{
+      outline: none;
+      color: rgba(0,0,0,0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child{
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img{
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button{
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0,0,0,0.15); 
+        border-radius: 35px;
+        background-color: white;
+        text-align: left;
+      }
+    }
+    &:nth-child(2){
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      
+      button{
+        img{
+          margin: 0 5px 0 -2px;
+        }
+        span{
+          color: #70b5f9;
+        }
+      }
+    }
+  }
+
+`;
+
+export default Main
